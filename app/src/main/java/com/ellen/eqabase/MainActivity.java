89ef2604ed55561d15d3cf0f76727682288f88fd
陈浪 <chenlang@sx985.com>
2020-05-21 10:37:39 +0800

    加入File操作封装

--- app/src/main/java/com/ellen/eqabase/MainActivity.java
+++ app/src/main/java/com/ellen/eqabase/MainActivity.java
@@ -1,11 +1,17 @@
 package com.ellen.eqabase;
 
-import android.view.View;
+import android.util.Log;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
 import com.ellen.baselibrary.eqa.base.BaseActivity;
+import com.ellen.baselibrary.eqa.save.file.AndroidFailePath;
+import com.ellen.baselibrary.eqa.uitil.PermissionUtils;
 
 public class MainActivity extends BaseActivity {
+
+    private PermissionUtils permissionUtils;
 
     @Override
     protected int setLayoutId() {
@@ -24,12 +30,30 @@ public class MainActivity extends BaseActivity {
 
     @Override
     protected void initData() {
+        permissionUtils = new PermissionUtils(this);
+        permissionUtils.startCheckFileReadWritePermission(0, new PermissionUtils.PermissionCallback() {
+            @Override
+            public void success() {
+                Log.e("Ellen2018", "外部存储目录:"+AndroidFailePath.getExternalPath());
+                Log.e("Ellen2018", "内部存储目录:"+AndroidFailePath.getInternalPath(MainActivity.this));
+            }
 
+            @Override
+            public void failure() {
+
+            }
+        });
     }
 
     @Override
     protected boolean back() {
         Toast.makeText(this,"点击了返回键",Toast.LENGTH_SHORT).show();
         return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        permissionUtils.onRequestPermissionsResult(requestCode,permissions,grantResults);
     }
 }
