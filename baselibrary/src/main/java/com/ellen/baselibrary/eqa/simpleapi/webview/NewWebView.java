a568bbb189ec98ee6cd4e991109005a0b70e5236
陈浪 <chenlang@sx985.com>
2020-05-24 17:18:38 +0800

    对WebViewCachePool进行修改

--- baselibrary/src/main/java/com/ellen/baselibrary/eqa/simpleapi/webview/NewWebView.java
+++ baselibrary/src/main/java/com/ellen/baselibrary/eqa/simpleapi/webview/NewWebView.java
@@ -3,11 +3,22 @@ package com.ellen.baselibrary.eqa.simpleapi.webview;
 import android.content.Context;
 import android.webkit.WebView;
 
+/**
+ * 如果你的项目中新生成的是自定义的WebView,那么就需要:
+ * WebViewCachePool.setNewWebView来进行自定义设置
+ */
 public interface NewWebView {
     /**
      * 获取当一个WebView对象
      * @return
      */
     WebView getNewWebView(Context context);
+
+    /**
+     * WebViewCachePool里最大的WebViw数量
+     * 默认值为：10
+     * 此方法是修改最大数量的唯一途径
+     * @return
+     */
     int maxSize();
 }
